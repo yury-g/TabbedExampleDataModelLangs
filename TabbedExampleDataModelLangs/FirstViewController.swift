e72a032c8ad5da716af082500624afc0771f44ed
yury-g <yury.gitman@gmail.com>
2016-04-28 09:29:16 -0400

    first basic build

---
+++ TabbedExampleDataModelLangs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedExampleDataModelLangs
+//
+//  Created by YG on 4/28/16.
+//  Copyright © 2016 YuryG. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
